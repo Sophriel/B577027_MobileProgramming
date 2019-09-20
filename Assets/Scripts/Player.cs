9e90423ca7165731f42ad9dfdf7fe192d5ec7763
Sophriel <32302066+Sophriel@users.noreply.github.com>
2019-09-20 23:09:27 +0900

    과제 1 가장 가까운 적 찾기
    Assets 폴더와 ProjectSettings 폴더의 파일들 및 전체 프로젝트 압축파일

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform closestZombie;
+
+    float searchRadius = 0.3f;
+    int layerMask;
+
+    void Start()
+    {
+        closestZombie = transform;
+
+        //  좀비만 탐색
+        layerMask = 1 << LayerMask.NameToLayer("Zombie");
+    }
+
+    void Update()
+    {
+        ZombieRador();
+    }
+
+    //  SphereCast를 이용하여 
+    //  가장 가까운 좀비를 탐색
+    public void ZombieRador()
+    {
+        //  구형 탐색
+        RaycastHit[] hit = Physics.SphereCastAll(transform.position, searchRadius, Vector3.up, 0.0f, layerMask);
+
+        if (hit.Length > 0)
+        {
+            //  탐지시, 기존의 좀비를 흰색으로 변경하고
+            closestZombie.GetComponent<MeshRenderer>().material.color = Color.white;
+
+            closestZombie = hit[0].transform;
+
+            //  더 가까운 좀비를 빨간색으로 변경
+            closestZombie.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            searchRadius = 0.3f;
+        }
+
+        else
+        {
+            //  탐색 반경이 늘어남
+            searchRadius += 0.1f;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        //  SphereCast를 시각화
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+    }
+}
---
+++ Assets/Scripts/ZombieManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : MonoBehaviour
+{
+    public Transform PlayerCharacter;
+
+    public GameObject ZombiePrafab;
+    public List<Transform> ZombieTransforms;
+
+    private Transform closestZombie;
+
+    void Start()
+    {
+        //  플레이어 캐릭터 할당
+        PlayerCharacter = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //  좀비 생성 후 리스트에 추가
+        for (int i = 0; i < 10; i++)
+        {
+            float posX = Random.Range(-10.0f, 10.0f);
+            float posZ = Random.Range(-10.0f, 10.0f);
+            Transform zombie = Instantiate(ZombiePrafab, new Vector3(posX, 0.5f, posZ), Quaternion.identity).transform;
+
+            ZombieTransforms.Add(zombie);
+        }
+
+        //  초기화
+        closestZombie = ZombieTransforms[0];
+    }
+
+    void Update()
+    {
+        //CheckZombiesDistance();
+    }
+
+    //  리스트에 존재하는 좀비들과 플레이어 사이의 거리를 측정하여
+    //  가장 가까운 좀비를 탐색
+    public void CheckZombiesDistance()
+    {
+        //  기존 빨간 좀비의 색상 변경
+        closestZombie.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+
+        //  플레이어와 좀비들의 거리를 비교
+        foreach (Transform z in ZombieTransforms)
+        {
+            if ((PlayerCharacter.position - z.position).sqrMagnitude <
+                (PlayerCharacter.position - closestZombie.position).sqrMagnitude)
+            {
+                closestZombie = z;
+            }
+        }
+
+        //  가장 가까운 좀비의 색상 변경
+        closestZombie.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
